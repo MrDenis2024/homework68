d1f3030572f0d773fb777d446e50dda70b47c641
Khrunev Denis <deniskhrunev@gmail.com>
2024-07-15 16:12:46 +0600

    Созданы компоненты store b todoSlice для рабыты с глобальным стейтом

---
+++ src/Todo/todoSlice.ts
@@ -0,0 +1,11 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+export const todoSlice = createSlice({
+  name: "todo",
+  initialState: {},
+  reducers: {
+
+  }
+});
+
+export const todoReducer = todoSlice.reducer;
